74caf84d728072347407a394b2999452e99899cc
Zahid <Zahiddin9423@gmail.com>
2023-11-15 22:07:26 +0400

    header styl

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import Header from "./components/header/header";
-import Container from '@mui/material/Container';
 
 
 function App() {
   return (
     <div className='App'>
-      <Container maxWidth='sm'>
-        <Header />
-      </Container>
-      
+      <Header />
     </div>
   );
 }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,23 +1,33 @@
 import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import "./header.css";
 
 const Header = () => {
   return (
     <div>
-      <div className='logo'></div>
-      <div className='head_nav' sx={{
-        // color: #EFE7E2,
-        // fontFamily: Fira Sans,
-        // fontsize: 15,
-        // fontstyle: normal,
-        // fontweight: 400,
-        // lineheight: 135%
-      }}>
-        <Button variant='text'>Text</Button>
-        <Button variant='text'>Text</Button>
-        <Button variant='text'>Text</Button>
-        <Button variant='text'>Text</Button>
-        <Button variant='text'>Text</Button>
-      </div>
+      <Container maxWidth='xl'>
+        <div className='wrapper'>
+          <div className='logo'>
+            <Button variant='text'>Cool Barber</Button>
+          </div>
+          <div className='head_nav'>
+            <Button variant='text'>Text</Button>
+            <Button variant='text'>Text</Button>
+            <Button variant='text'>Text</Button>
+            <Button variant='text'>Text</Button>
+            <Button variant='text'>Text</Button>
+          </div>
+          <div className='head_contacts'>
+            <div >
+              <Button variant='text'>302 W 50th St, NY, NY 10019</Button>
+            </div>
+            <div className="contact_tel">
+              <Button className="btn_tel" variant='text'>8 (800) 000-00-00</Button>
+              <Button className="btn_tel" variant='text'>Daily from 9 to 20</Button>
+            </div>
+          </div>
+        </div>
+      </Container>
     </div>
   );
 };
